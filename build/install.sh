6a650546213ab375dd37e5e8ef67e69059cb1b19
Mike Tiurin <mikhail.tiurin@easybrain.com>
2021-11-11 20:15:20 +0200

    minor: remove uneeded files

--- build/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -f dist/http_exporter /usr/local/bin/
-mkdir -p /etc/http_exporter
-cp config/config.yaml.tpl /etc/http_exporter/config.yaml
-cp init/http_exporter.service /etc/systemd/system
-systemctl enable http_exporter.service
